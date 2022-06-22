6bf014b1fd9dd9929b3ea3ab93b1b06766b5c5ca
Joey Drenth <drenthjoey@gmail.com>
2022-06-22 16:31:47 +0200

    Add create button to index pages

--- resources/views/teams/index.blade.php
+++ resources/views/teams/index.blade.php
@@ -46,5 +46,11 @@
                 </x-table.data>
             </x-table.row>
         @endforeach  
-    </x-table>  
+    </x-table>
+    <div class="float-right">
+        <a href="{{ route('teams.create') }}"
+            class="mt-6 px-4 py-2 inline-flex items-center justify-center rounded-md border border-transparent text-white bg-amber-600 hover:bg-amber-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-amber-500">
+            Toevoegen
+        </a>
+    </div>
 @endsection
